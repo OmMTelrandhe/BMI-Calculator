e9f5914f62161fbb9213db62f9ec5e7efea63fd0
OmMTelrandhe <139479968+OmMTelrandhe@users.noreply.github.com>
2024-04-20 20:38:07 +0530

    Add files via upload
    Home Page

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-function redirectToIndex() {
-    if(document.getElementById("email").value == "ayot@example.com" && document.getElementById("pass").value == "123456"){
-    window.location.href = '/HOME/index.html';
+let btn = document.getElementById("calculate");
+
+btn.addEventListener("click", () => {
+
+    let height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
+
+    if (height === "" || weight === "") {
+        alert("Please enter valid values");
+        return;
     }
-}
+
+    // BMI = w in kg and height in  m
+
+    height = height / 100
+    let bmi = (weight / (height * height)).toFixed(2);
+
+    document.getElementById("bmi").innerText = bmi;
+
+    let Cat = document.getElementById("status");
+            if (bmi < 18.5) {
+                Cat.innerText = "Underweight";
+                Cat.style.color = "darkgoldenrod";
+                Cat.style.fontWeight = "bold";
+                Cat.style.fontSize = "25px";
+            } else if (bmi >= 18.5 && bmi <= 24.9) {
+                Cat.innerText = "Normal";
+                Cat.style.color = "Darkgreen";
+                Cat.style.fontWeight = "bold";
+                Cat.style.fontSize = "25px";
+            } else if (bmi >= 25 && bmi <= 29.9) {
+                Cat.innerText = "Overweight";
+                Cat.style.color = "lightcoral";
+                Cat.style.fontWeight = "bold";
+                Cat.style.fontSize = "25px";
+            } else {
+                Cat.innerText = "Obese";
+                Cat.style.color = "darkred";
+                Cat.style.fontWeight = "bold";
+                Cat.style.fontSize = "25px";
+            }
 
 
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+})
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
 
-// function auth(){
-//     var email = document.getElementById("email").value;
-//     var password = document.getElementById("pass").value;
-
-//     if(email=="ayot@example.com" && password=="123456"){
-//         alert("Please Enter Email");
-//         window.location.assign = "/HOME/iho.html";
-//     }
-//     else{
-//         alert("Please Enter Valid input");
-//         return;
-//     }
-    
-// }
